382bc150a9485c858b7287b8d57699c3b17e1632
Param Chandarana <parambchandarana@gmail.com>
2024-06-07 20:27:38 +0530

    Remove Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "https://offer-letter-generation.vercel.app",
+    // origin: process.env.CORS_ORIGIN,
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
