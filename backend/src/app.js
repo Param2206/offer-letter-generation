92508f530ce007a0a26347c08ed5214a26cd754c
Param Chandarana <parambchandarana@gmail.com>
2024-06-07 20:19:31 +0530

    Fix the URLs

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "https://offer-letter-generation.vercel.app",
     credentials: true,
   })
 );
