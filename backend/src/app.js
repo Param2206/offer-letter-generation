afcc322786b099ca2b72c6dbab8a89f14e5a8257
Param Chandarana <parambchandarana@gmail.com>
2024-06-10 13:44:57 +0530

    make pre production changes again

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,43 +1,29 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
-import { fileURLToPath } from "url";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
-// Derive __dirname from import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// app.use(
-//   cors({
-//     // origin: process.env.CORS_ORIGIN,
-//     origin: "https://offer-letter-generation.onrender.com",
-//     credentials: true,
-//   })
-// );
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-});
-
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    // origin: "https://offer-letter-generation.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// Import routers
 import courseRouter from "./routes/course.routes.js";
 import studentRouter from "./routes/student.routes.js";
 import userRouter from "./routes/user.routes.js";
 import pdfRouter from "./routes/pdf.routes.js";
 import lastIdRouter from "./routes/lastId.routes.js";
 
-// Use routers
 app.use("/api/courses", courseRouter);
 app.use("/api/students", studentRouter);
 app.use("/api/users", userRouter);
@@ -48,7 +34,6 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
