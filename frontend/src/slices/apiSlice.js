7d82e5282633b9744ef298421b42fec175baffd9
Param Chandarana <parambchandarana@gmail.com>
2024-06-10 17:53:39 +0530

    fix the API URLs in frontend 2

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({ baseUrl: "https://offer-letter-generation-api.onrender.com" });
 
 export const apiSlice = createApi({
   baseQuery,
